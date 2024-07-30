9653b6284d2a6e8424e8f46d295bc0a82933627d
thesooge <thesooge.8313@gmail.com>
2024-07-30 18:25:18 +0330

    products app and model added

---
+++ products/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.shortcuts import reverse
+
+# Create your models here.
+
+class Product(models.Model):
+
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    status = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('#' ,args=[self.id])
