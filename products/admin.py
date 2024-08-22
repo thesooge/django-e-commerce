8ddb7582beac5f713b51273e9a299f76997aef54
thesooge <thesooge.8313@gmail.com>
2024-08-22 17:41:40 +0330

    add tabularadmin to products

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,24 @@ from django.contrib import admin
 from .models import Product, ProductComment
 
 # Register your models here.
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'status']
+
+# @admin.register(ProductComment)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['product', 'author', 'stars']    
+
+# Inline class for ProductComment
+class ProductCommentInline(admin.TabularInline):
+    model = ProductComment
+    extra = 1  # Number of empty forms to display
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
+    inlines = [ProductCommentInline]  # Include ProductComment as inline
 
 @admin.register(ProductComment)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['product', 'author', 'stars']    
+class ProductCommentAdmin(admin.ModelAdmin):  # Corrected the class name
+    list_display = ['product', 'author', 'stars']
